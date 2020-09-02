ce84b8a31587956950ad1a5619144faf0950b0d5
yungweezy <choonweiw@gmail.com>
2020-09-02 23:22:26 +0800

    reinitialise all files

--- lab1/ex1/node.c
+++ lab1/ex1/node.c
@@ -2,7 +2,7 @@
 * Lab 1 Exercise 1
 * Name: Wong Choon Wei
 * Student No: A0202190X
-* Lab Group:
+* Lab Group: 11
 *************************************/
 
 #include <stdio.h>
@@ -10,82 +10,116 @@
 #include <string.h>
 #include <ctype.h>
 #include "node.h"
+#include <stdbool.h>
 
 // add in your implementation below to the respective functions
 // feel free to add any headers you deem fit (although you do not need to)
+
+node* search_node(list *lst, int index, bool front) {
+  // initialise search node
+  node* searchedNode = (node*) malloc(sizeof(node));
+  searchedNode = front ? lst->head : lst->tail;
+
+  for(int i = 0; i < index; i++) {
+    if (front) {
+      searchedNode = searchedNode->next;
+    } else {
+      searchedNode = searchedNode->prev;
+    }
+  }
+  return searchedNode;
+}
 
 // inserts a new node with data value at index (counting from the front
 // starting at 0)
 void insert_node_from_head_at(list *lst, int index, int data)
 {
-  list *originalList = (list *)malloc(sizeof(list));
-  originalList = lst;
-  if (lst->head == NULL) {
-    node* newNode = (node*) malloc(sizeof(node));
-    newNode->data = data;
-    newNode-> prev = NULL;
-    newNode->next = NULL;
-    lst->head = newNode;
-    lst->tail = lst->head;
+  node* nextNode = (node*) malloc(sizeof(node));
+  node* prevNode = (node*) malloc(sizeof(node));
 
+  node* newNode = (node*) malloc(sizeof(node));
+
+  if (index != 0) {
+    prevNode = search_node(lst, index - 1, true);
   } else {
-    if(index == 0) {
-    // initialise new node
-    node* newNode = (node*) malloc(sizeof(node));
-    newNode->data = data;
-    newNode->prev = lst->head->prev;
-    newNode->next = lst->head;
-
-    // change prev node
-    lst->head->prev->next = newNode;
-    
-    //change index node
-    lst->head->prev = newNode; 
-
-    } else {
-      list *newList = (list *)malloc(sizeof(list));
-      newList->head = lst->head->next;
-      insert_node_from_head_at(newList, index - 1, data);
-    }
+    prevNode = NULL;
   }
-  lst = originalList;
+  nextNode = search_node(lst, index, true);
+
+  // set newNode
+  newNode->data = data;
+  newNode->prev = prevNode;
+  newNode->next = nextNode;
+
+  // set prevNode
+  if (prevNode != NULL) {
+    prevNode->next = newNode;
+  }
+
+  // set nextNode
+  if (nextNode != NULL) {
+    nextNode->prev = newNode;
+  }
+
+  if (prevNode == NULL && nextNode == NULL) {
+    lst->head = newNode;
+    lst->tail = newNode;
+  }
+
+  // reorder list
+  if (index == 0) {
+    lst->head = newNode;
+  }
+
+  if (index != 0 && nextNode == NULL) {
+    lst->tail = newNode;
+  }
+
 }
 
 // inserts a new node with data value at index (counting from the back
 // starting at 0)
 void insert_node_from_tail_at(list *lst, int index, int data)
 {
-  list *originalList = (list *)malloc(sizeof(list));
-  originalList = lst;
-  if (lst->head == NULL) {
-    node* newNode = (node*) malloc(sizeof(node));
-    newNode->data = data;
-    newNode-> prev = NULL;
-    newNode->next = NULL;
-    lst->head = newNode;
-    lst->tail = lst->head;
+  node* nextNode = (node*) malloc(sizeof(node));
+  node* prevNode = (node*) malloc(sizeof(node));
+  node* newNode = (node*) malloc(sizeof(node));
 
+  if (index != 0) {
+    nextNode = search_node(lst, index - 1, false);
   } else {
-    if(index == 0) {
-    // initialise new node
-    node* newNode = (node*) malloc(sizeof(node));
-    newNode->data = data;
-    newNode->prev = lst->tail->prev;
-    newNode->next = lst->tail;
-
-    // change prev node
-    lst->tail->prev->next = newNode;
-    
-    //change index node
-    lst->tail->prev = newNode; 
-
-    } else {
-      list *newList = (list *)malloc(sizeof(list));
-      newList->tail = lst->tail->prev;
-      insert_node_from_head_at(newList, index - 1, data);
-    }
+    nextNode = NULL;
   }
-  lst = originalList;
+  prevNode = search_node(lst, index, false);
+
+  // set newNode
+  newNode->data = data;
+  newNode->prev = prevNode;
+  newNode->next = nextNode;
+
+  // set prevNode
+  if (prevNode != NULL) {
+    prevNode->next = newNode;
+  }
+
+  // set nextNode
+  if (nextNode != NULL) {
+    nextNode->prev = newNode;
+  }
+
+  if (prevNode == NULL && nextNode == NULL) {
+    lst->head = newNode;
+    lst->tail = newNode;
+  }
+
+  // reorder list
+  if (index == 0) {
+    lst->tail = newNode;
+  }
+
+  if (index != 0 && prevNode == NULL) {
+    lst->head = newNode;
+  }
 
 }
 
@@ -93,55 +127,108 @@ void insert_node_from_tail_at(list *lst, int index, int data)
 // note: index is guaranteed to be valid
 void delete_node_from_head_at(list *lst, int index)
 {
-  if(lst->head == NULL) {
-    if(lst->tail != NULL) {
+  node* nextNode = (node*) malloc(sizeof(node));
+  node* prevNode = (node*) malloc(sizeof(node));
+  node* deleteNode = (node*) malloc(sizeof(node));
 
-    }
+  deleteNode = search_node(lst, index, true);
+
+  // get prevNode
+  if (index != 0) {
+    prevNode = search_node(lst, index - 1, true);
   } else {
-    if (index == 0) {
-      node* newNode = (node*) malloc(sizeof(node));
-      newNode = lst->head;
-      // set correct previous head
-      lst->head->next->prev = lst->head->prev;
+    prevNode = NULL;
+  }
 
-      if(lst->head->prev != NULL) {
-        lst->head->prev->next = lst->head->next;
-      }
+  // get nextNode
+  if (deleteNode != NULL) {
+    nextNode = deleteNode->next;
+  } else {
+    nextNode = NULL;
+  }
 
-      free(newNode);
-    } else {
-      list *newList = (list *)malloc(sizeof(list));
-      newList->head = lst->head->next;
-      delete_node_from_head_at(newList, index - 1);
+  //set nodes
+  if (prevNode != NULL) {
+     prevNode->next = nextNode;
+  }
+
+  if (nextNode!= NULL) {
+    nextNode->prev = prevNode;
+  }
+
+  // set list
+  if (index == 0) {
+    lst->head = nextNode;
+  } else {
+    if (deleteNode == NULL || nextNode == NULL) {
+      lst->tail = prevNode;
     }
   }
+
+  // free deleteNode
+  free(deleteNode);
+
+
 }
 
 // deletes node at index counting from the back (starting from 0)
 // note: index is guaranteed to be valid
 void delete_node_from_tail_at(list *lst, int index)
 {
-  if(lst->head == NULL) {
+  node* nextNode = (node*) malloc(sizeof(node));
+  node* prevNode = (node*) malloc(sizeof(node));
+  node* deleteNode = (node*) malloc(sizeof(node));
+
+  deleteNode = search_node(lst, index, false);
+
+  // get nextNode
+  if (index != 0) {
+    nextNode = search_node(lst, index - 1, false);
   } else {
-    if (index == 0) {
-      node* newNode = (node*) malloc(sizeof(node));
-      newNode = lst->tail;
-      // set correct previous head
-      lst->tail->next->prev = lst->head->prev;
-      
-      free(newNode);
-    } else {
-      list *newList = (list *)malloc(sizeof(list));
-      newList->tail = lst->tail->prev;
-      delete_node_from_tail_at(newList, index - 1);
+    nextNode = NULL;
+  }
+
+  // get prevNode
+  if (deleteNode != NULL) {
+    prevNode = deleteNode->prev;
+  } else {
+    prevNode = NULL;
+  }
+
+  //set nodes
+  if (prevNode != NULL) {
+     prevNode->next = nextNode;
+  }
+
+  if (nextNode!= NULL) {
+    nextNode->prev = prevNode;
+  }
+
+  // set list
+  if (index == 0) {
+    lst->tail = prevNode;
+  } else {
+    if (deleteNode == NULL || prevNode == NULL) {
+      lst->head = nextNode;
     }
   }
+
+  // free deleteNode
+  free(deleteNode);
 }
 
 // resets list to an empty state (no nodes) and frees any allocated memory in
 // the process  
 void reset_list(list *lst)
 {
+  node *curr = lst->head;
+  while (curr != NULL)
+    {
+        node *releaseNode = curr;
+        curr = curr->next;
+        free(releaseNode);
+    }
+  
   lst->head = NULL;
   lst->tail = NULL;
 }
